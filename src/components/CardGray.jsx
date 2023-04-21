6a1698f69dbe0a5d90cfc560e4928cf5cee6a7cc
David-Koshkadze <rootUS3rr@pm.me>
2023-04-21 18:31:20 +0400

    some changes 1

--- src/components/CardGray.jsx
+++ src/components/CardGray.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function CardGray({ position, voucher, prize }) {
   return (
-    <div className="bg-[#1E2122] rounded-full border-2 border-[#2C3234] h-[56px] flex items-center justify-between pl-5 pr-9 mb-0.5">
+    <div className="bg-[#1E2122] rounded-full border-2 border-[#2C3234] h-[56px] flex items-center justify-between pl-5 pr-9 mb-0.5 ">
       <div
         className="rounded-full w-7 h-7 bg-[#2C3234] flex items-center justify-center"
         style={{
---
+++ src/components/VoucherCard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function VoucherCard() {
+  return (
+    <div className="ticket-voucher-card bg-[#202324] w-full h-[72px] rounded-[12px] relative px-8 flex items-center justify-start">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
